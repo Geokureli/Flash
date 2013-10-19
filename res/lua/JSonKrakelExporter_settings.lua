9c58efa17959c2796911e5594eaa195b76d7b526
Geokureli <Gkurelic@gmail.com>
2013-10-19 01:16:08 -0500

    moved lua files to res

---
+++ res/lua/JSonKrakelExporter_settings.lua
@@ -0,0 +1,8 @@
+-- Display the settings for the exporter.
+
+DAME.AddBrowsePath("directory:", "DataDir", false, "Where you place the data files.")
+
+DAME.AddCheckbox("Export only CSV","ExportOnlyCSV",false,"If ticked then the script will only export the map CSV files and nothing else.")
+DAME.AddCheckbox("Export Sprite angle","EXPORT_ANGLE",false,"")
+
+return 1
